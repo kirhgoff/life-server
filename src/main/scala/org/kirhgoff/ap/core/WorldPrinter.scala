3576603de5926c207be842db7c8ddb8364cd6d3b
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2014-12-31 13:32:11 +0300

    added image generation

--- src/main/scala/org/kirhgoff/ap/core/WorldPrinter.scala
+++ src/main/scala/org/kirhgoff/ap/core/WorldPrinter.scala
@@ -1,9 +1,12 @@
 package org.kirhgoff.ap.core
 
-/**
- * Created by kirilllastovirya on 14/12/14.
- */
+import java.awt.Color
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
 class WorldPrinter(val world:WorldModel, val aliveSymbol:Char, val deadSymbol:Char) {
+  var counter = 0
+  var path = "c:/Tmp/images/"
 
   def printEndOfTheWorld() = {
     println ("============================>End of the world")
@@ -18,6 +21,29 @@ class WorldPrinter(val world:WorldModel, val aliveSymbol:Char, val deadSymbol:Ch
       }
     }
     asci.map(row => row.toList.mkString("")).toList.mkString("\n")
+  }
+
+  def createPicture (elements:List[Element]) = {
+    val screenWidth = 500
+    val screenHeight = 500
+    val cellWidth = screenWidth/world.width
+    val cellHeight = screenHeight/world.height
+
+    val canvas = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_RGB)
+    val g = canvas.createGraphics()
+    g.setColor(Color.WHITE)
+    g.fillRect(0, 0, canvas.getWidth, canvas.getHeight)
+
+    g.setColor(Color.BLACK)
+    elements.map(
+      element => {
+        if (element.isAlive) g.fillRect(element.x*cellWidth, element.y*cellHeight, cellWidth, cellHeight)
+      }
+    )
+
+    g.dispose()
+    ImageIO.write(canvas, "png", new java.io.File(path + "world" + ))
+
   }
 
 }
