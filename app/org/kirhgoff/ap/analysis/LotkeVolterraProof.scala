f2fd8c123875b743588669f9fe7f1a3b7f697605
Kirill Lastovirya <kirill.lastovirya@moex.com>
2015-08-17 12:17:01 +0300

    made project compile

--- app/org/kirhgoff/ap/analysis/LotkeVolterraProof.scala
+++ app/org/kirhgoff/ap/analysis/LotkeVolterraProof.scala
@@ -5,39 +5,44 @@ package org.kirhgoff.ap.analysis
  */
 
 import slick.driver.H2Driver.api._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object LotkeVolterraProof {
   def main(a:Array[String]) {
     val db = Database.forConfig("h2mem1")
     try {
 
-      class GameStepResults(tag: Tag) extends Table[(Int, Int, Int)](tag, "LOTKE_VOLTERRA") {
-        def id = column[Int]("STEP_ID", O.PrimaryKey) // This is the primary key column
+      class GameStepResults(tag: Tag) extends Table[(String, Int, Int)](tag, "LOTKE_VOLTERRA") {
+        def id = column[String]("STEP_ID", O.PrimaryKey) // This is the primary key column
         def hunters = column[Int]("HUNTERS")
         def preys = column[Int]("PREYS")
         // Every table needs a * projection with the same type as the table's type parameter
         def * = (id, hunters, preys)
       }
       val results = TableQuery[GameStepResults]
+
       val setup = DBIO.seq(
         results.schema.create,
-        results +=(101, 1, 10),
-        results +=(49, 2, 8),
-        results +=(150, 3, 5)
+
+        results +=("1", 1, 10),
+        results +=("2", 2, 8),
+        results +=("3", 3, 5)
       )
 
       val setupFuture = db.run(setup)
-      println("Results:")
+//      println("Results:")
 //      db.run(results.result).map(_.foreach {
 //        case (name, supID, price, sales, total) =>
 //          println("  " + name + "\t" + supID + "\t" + price + "\t" + sales + "\t" + total)
 //      })
-// Why not let the database do the string conversion and concatenation?
-    val q1 = for(r <- results)
-      yield LiteralColumn("  ") ++ r.id.asColumnOf[Int] ++
-    "\t" ++ r.hunters.asColumnOf[Int] ++
-    "\t" ++ r.price.asColumnOf[Int]
-      db.stream(q1.result).foreach(println)  }
+
+      // val q1 = for(r <- results)
+      //   yield LiteralColumn("  ") ++ r.id.asColumnOf[String] ++
+      // "\t" ++ r.hunters.asColumnOf[Int] ++
+      // "\t" ++ r.price.asColumnOf[Int]
+      //   db.stream(q1.result).foreach(println)  
+    } finally db.close
+  }
 }
 
 
